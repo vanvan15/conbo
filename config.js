105dd1802e7598d1b2cd4342574a4227b242b23b
vanvan15 <117937283+vanvan15@users.noreply.github.com>
2022-11-14 18:57:22 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Trà My';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng Bạn';
+const contentCard = 'Chúc chú bé béo tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bạn trong hôm nay và cả những ngày sau';
+
+
+const giftImage = 'tramy.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
